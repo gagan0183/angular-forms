e01a25700ba0fd477e4e194a3f3a840406e37b14
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-14 18:03:39 +0530

    form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { UserSettingsFormComponent } from "./user-settings-form/user-settings-form.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserSettingsFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
