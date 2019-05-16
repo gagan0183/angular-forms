2209846f539923db9c7bfdb5574b5311bec738ff
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-16 21:22:10 +0530

    created data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { DataService } from "./data/data.service";
 import { UserSettingsFormComponent } from "./user-settings-form/user-settings-form.component";
 
 @NgModule({
   declarations: [AppComponent, UserSettingsFormComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/data/data.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+import { UserSettings } from "./user-settings";
+
+@Injectable({})
+export class DataService {
+  constructor() {}
+
+  postUserSettingsFrom(userSettings: UserSettings) {}
+}
